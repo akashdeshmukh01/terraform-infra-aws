455857087d3fdc6f5f1fded23b29e40d2bc24525
akashdeshmukh01 <74890480+akashdeshmukh01@users.noreply.github.com>
2025-04-05 21:42:11 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,65 @@
+variable "aws_region" {
+  description = "This is the aws regon, where all the infra will get created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway for private subnets"
+  type        = bool
+}
+
+variable "vpc_name" {
+
+description = "Name of the VPC"
+  type        = string
+}
+
+variable "node_group_name" {
+  description = "Name of the node group"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "EKS Cluster name"
+  type        = string
+}
+
+variable "env" {
+
+    description = "EKS Cluster var name"
+  type        = string
+  
+}
